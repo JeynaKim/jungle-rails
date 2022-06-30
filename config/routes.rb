dbf071955cfec72ea28f9ccd701f4126dfd0e5a2
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-30 22:03:46 +0000

    Complete Feature: User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  # These routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
