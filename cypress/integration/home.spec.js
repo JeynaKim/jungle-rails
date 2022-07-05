037057a6779d85d62c90e6b70f7a6bd46358f48d
Jeyna Kim <jeyna.kimm@gmail.com>
2022-07-05 19:29:27 +0000

    Complete Feature Spec: Product Details

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,15 +1,13 @@
-describe('Main', () => {
+describe('Homepage', () => {
   it('should be able to visit the root page', () => {
     cy.visit('/');
   });
 
   it("There is products on the page", () => {
-    cy.visit('/');
     cy.get(".products article").should("be.visible");
   });
 
   it("There is 2 products on the page", () => {
-    cy.visit('/');
     cy.get(".products article").should("have.length", 2);
   });
 })
---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,14 @@
+describe('Product details', () => {
+  it('should be able to visit the root page', () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("User can click and visit to the product detail page", () => {
+    cy.get(".products article").first().click()
+    cy.get('.product-detail').should("be.visible");
+  });
+})
